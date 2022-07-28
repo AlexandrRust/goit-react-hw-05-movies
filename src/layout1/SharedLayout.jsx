7b732b90996ac6c9acd97c94161d88cf48813251
AlexandrRust <123_00@ukr.net>
2022-07-28 06:13:01 +0300

    fix

--- src/layout1/SharedLayout.jsx
+++ src/layout1/SharedLayout.jsx
@@ -1,6 +1,6 @@
 import { Header } from 'components/Header/Header.styled';
-import { Nav } from 'Layout/comons/Nav.styled';
-import { NavItem } from 'Layout/comons/NavItem.styled';
+import { Nav } from 'layout1/comons/Nav.styled';
+import { NavItem } from 'layout1/comons/NavItem.styled';
 import { Main } from './comons/Main.styled';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
